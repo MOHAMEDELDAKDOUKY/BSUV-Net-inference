cdb7968f1b485636b7c22d32056e7284ab20fe99
Abdelrahman350 <eng.abdelrahman.ahmad@gmail.com>
2022-03-29 11:43:48 +0200

    edit

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -160,7 +160,8 @@ class videoLoader(data.IterableDataset):
     def __readRGB(self, path):
         assert os.path.exists(path), "{} does not exist".format(path)
         im = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB).astype(np.float)/255
-        im = cv2.resize(im, (720, 576), interpolation= cv2.INTER_AREA)
+        # (720, 576)
+        im = cv2.resize(im, (320, 240), interpolation= cv2.INTER_AREA)
 
         h, w, _ = im.shape
         h_valid = int(h / 16) * 16
@@ -168,7 +169,7 @@ class videoLoader(data.IterableDataset):
         return im[:h_valid, :w_valid, :]
 
     def __preProc(self, fr):
-        fr = cv2.resize(fr, (720, 576), interpolation= cv2.INTER_AREA)
+        fr = cv2.resize(fr, (320, 240), interpolation= cv2.INTER_AREA)
         h, w, _ = fr.shape
         h_valid = int(h / 16) * 16
         w_valid = int(w / 16) * 16
